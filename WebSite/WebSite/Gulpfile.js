a5198b2bc8ef550179e0afeac48c8bcbe8a51044
shteniovych <victor.shteniovych@gmail.com>
2018-12-27 14:18:55 +0200

    responsive

---
+++ WebSite/WebSite/Gulpfile.js
@@ -0,0 +1,20 @@
+﻿/// <binding Clean='clean' />
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    path = require('path');
+
+var paths = {
+    css: "./wwwroot/css",
+    sass: "./wwwroot/sass"
+};
+
+
+
+// prepare LESS files
+gulp.task("sass-to-css", function () {
+    return gulp
+        .src(paths.sass + "/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest(paths.css));
+});
+
